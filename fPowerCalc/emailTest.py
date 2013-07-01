0a92cd63d8c6018c4426a45e6a055bb50e08ff54
Liron Shimrony <lsxliron@gmail.com>
2013-07-01 01:31:08 -0400

    Fixed send email function- takes 5 arguments now, correct file in fpowercalc dir

---
+++ fPowerCalc/emailTest.py
@@ -0,0 +1,7 @@
+import sendEmaily as a
+
+def main():
+	a.send_mail('lsxliron@gmail.com', ' iviagxlnzhwqqizn ', '/Users/lsxliron/Desktop/test.c', 'SUBJECT', "BODY TEST")
+
+if __name__ == '__main__':
+	main()
---
+++ fPowerCalc/templates/emailTest.py
@@ -0,0 +1,7 @@
+import sendEmaily
+
+def main():
+	send_mail('lsxliron@gmail.com', ' iviagxlnzhwqqizn ', '/Users/lsxliron/test.c', 'SUBJECT', "BODY TEST")
+
+if __name__ == '__main__':
+	main()
